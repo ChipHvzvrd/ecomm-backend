1b5ce77ad8cbd809bf1293c341e05bb61ba4cf71
Christian Pena <Chip@Christians-MBP.attlocal.net>
2022-10-14 11:59:51 -0500

    seeds working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,19 @@
-INSERT INTO product_tag (id, product_id)
-VALUES
-(1, 1),
-(2, 2),
-(3, 3);
-
 INSERT INTO category (id, category_name)
+
 VALUES
-(1, 'Technology'),
-(2, 'Fantasy'),
-(3, 'Music');
+(1, "Technology");
+
+INSERT INTO product (id, product_name, price, stock, category_id)
+
+VALUES
+(1, "3DPrinter", 199.99, 12, 1);
+
+INSERT INTO product_tag (id, product_id)
+
+VALUES 
+(1, 1);
 
 INSERT INTO tag (id, tag_name)
-VALUES
-(1, 'Printer'),
-(2, 'Tesseract'),
-(3, 'Musician');
 
-INSERT INTO TABLE product (id, product_name, price, stock, category_id)
 VALUES
-(1, '3DPrinter', 199.99, 12, 1),
-(2, '4DPrinter', 19.99, 1, 2),
-(3, 'Guitar', 350.00, 3, 3);
+(1, "tech22");
